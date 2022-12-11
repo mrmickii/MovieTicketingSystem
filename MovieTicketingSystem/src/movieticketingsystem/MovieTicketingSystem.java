b0d0456e72c0a3dce43a17d6518498eff428f1e4
Carlo <Carlo@Carlo>
2022-12-11 21:20:28 +0800

    Created StartUp Form

--- MovieTicketingSystem/src/movieticketingsystem/MovieTicketingSystem.java
+++ MovieTicketingSystem/src/movieticketingsystem/MovieTicketingSystem.java
@@ -14,7 +14,9 @@ public class MovieTicketingSystem {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        LoginForm lf = new LoginForm();
+        lf.show();
     }
     
 }
